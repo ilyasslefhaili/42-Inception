29c3cb41c7abfefc63f9f4a9faa167d31b56b4b0
ilyasslefhaili <ilefhail@student.1337.ma>
2023-01-08 02:15:59 +0100

    add the ctop service

--- srcs/requirements/mariadb/tools/setup_db.sh
+++ srcs/requirements/mariadb/tools/setup_db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 service mysql start
 mysql -u root -e "CREATE DATABASE $MYSQL_DATABASE DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;CREATE USER $MYSQL_USER@'%' IDENTIFIED BY '"$MYSQL_PASSWORD"';GRANT ALL PRIVILEGES  ON  $MYSQL_DATABASE.* TO 'user'@'%';FLUSH PRIVILEGES;"
-sleep 5
+sleep 3
 mysql -u root  -e  "ALTER USER 'root'@'localhost' IDENTIFIED BY '"$MYSQL_ROOT_PASSWORD"'; flush privileges;"
 mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
 exec $@
