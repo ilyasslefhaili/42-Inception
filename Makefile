1504a22e81010a5c7494156cad1196aefc911852
ilyasslefhaili <ilefhail@student.1337.ma>
2023-01-13 21:43:15 +0100

    some updates

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 all: 
 	sudo chmod 666 /var/run/docker.sock
 	bash ./srcs/requirements/tools/create_dr.sh
-	docker compose -f  ./srcs/docker-compose.yml  up --build
-clean:
+	docker compose -f  ./srcs/docker-compose.yml  up -d
+down:
 	docker compose -f ./srcs/docker-compose.yml down
 re : clean all
--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -2,10 +2,6 @@
 
 define( 'WP_REDIS_HOST', 'redis' );
 define( 'WP_REDIS_PORT', 6379 );
-define( 'WP_REDIS_TIMEOUT', 1 );
-define( 'WP_REDIS_READ_TIMEOUT', 1 );
-define ('WP_REDIS_DATABASE', 0 );
-
 
 
 define( 'DB_NAME', 'wordpress' );
@@ -17,23 +13,13 @@ define( 'DB_USER', 'user' );
 define( 'DB_PASSWORD', 'pass' );
 
 /** Database hostname */
-define( 'DB_HOST', 'mariadb' );
+define( 'DB_HOST', 'mariadb:3306' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
 
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
-
-// define( 'AUTH_KEY',         'put your unique phrase here' );
-// define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-// define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-// define( 'NONCE_KEY',        'put your unique phrase here' );
-// define( 'AUTH_SALT',        'put your unique phrase here' );
-// define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-// define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-// define( 'NONCE_SALT',       'put your unique phrase here' );
-
 
 
 $table_prefix = 'wp_';
